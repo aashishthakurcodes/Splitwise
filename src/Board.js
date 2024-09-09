4bc8e8c3d77bc92ab9405be6be7780e72140808c
SHIVAM-YADAV-1012 <166971381+SHIVAM-YADAV-1012@users.noreply.github.com>
2024-09-09 15:42:57 +0530

    Filter

--- src/Board.js
+++ src/Board.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Expense from "./Expense";
-import { Container, Row, Col } from "react-bootstrap"; 
+import { Container, Row,  } from "react-bootstrap"; 
 import "./index.css";
 
 export default class Board extends React.Component {
@@ -9,15 +9,7 @@ export default class Board extends React.Component {
     this.state = {
       data: [
         {
-          name: "test1",
-          paidPerson: "Dion",
           amt: 0,
-          f1: 0,
-          f2: 0,
-          f3: 0,
-          f4: 0,
-          sp: 0,
-          sm: 0,
           money: 0,
         },
       ],
@@ -31,43 +23,7 @@ export default class Board extends React.Component {
     }));
   };
 
-  // Remove a bill from the state
-  removeComment = (index) => {
-    this.setState((prevState) => ({
-      data: prevState.data.filter((_, idx) => idx !== index),
-    }));
-  };
-
-  // Update a specific bill
-  updateComment = (
-    index,
-    newText,
-    newPP,
-    newAmt,
-    nf1,
-    nf2,
-    nf3,
-    nf4,
-    nsp,
-    nsm
-  ) => {
-    const newData = [...this.state.data];
-    newData[index] = {
-      name: newText,
-      paidPerson: newPP,
-      amt: newAmt,
-      f1: nf1,
-      f2: nf2,
-      f3: nf3,
-      f4: nf4,
-      sp: nsp,
-      sm: nsm,
-    };
-
-    this.setState({
-      data: newData,
-    });
-  };
+ 
 
   // Render each comment using the Expense component
   eachComment = (comment, index) => (
@@ -95,12 +51,7 @@ export default class Board extends React.Component {
                     name: "New Bill",
                     paidPerson: "Rajneesh",
                     amt: 0,
-                    f1: 0,
-                    f2: 0,
-                    f3: 0,
-                    f4: 0,
-                    sp: 0,
-                    sm: 0,
+                    
                     money: 0,
                   })
                 }
